14f84db10f2cb8892f6743b697e5af025d872ae9
songzhengxiang <songzhengxiang@yikongenomics.com>
2026-04-16 17:14:59 +0800

    add

---
+++ app/App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: #0a0a0f;
+		color: #f5f5f7;
+		font-family: -apple-system, 'Noto Sans SC', sans-serif;
+		min-height: 100%;
+	}
+
+	::-webkit-scrollbar {
+		width: 0;
+		height: 0;
+		background-color: transparent;
+	}
+</style>
